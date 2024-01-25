e86a25573d148195aec74074db75239d940b4312
Kenneth Treadaway <121038701+CyberKenneth@users.noreply.github.com>
2024-01-25 08:37:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,31 @@
 #Minerva Discord Bot by Kenneth Treadaway was created using the pycord library. Starting on 4/1/2023 at 2:20pm CST. This bot is a work in progress and is not yet complete. I will be adding more features as time goes on. This bot is not yet ready for public use.
 # If you would like to use this bot, please contact me at ken cyberresearch (dot) dev. I will be happy to help you get it up and running. I also will be happy to hear any suggestions you may have for this bot. Thank you for your interest in this bot. I hope you enjoy it.
 
-"""
-Importing the libraries
-Using the command "pip3 install pipreqs" and "pip-tools" to create the requirements.txt file
-    """
-import os
-import random
 import discord
-from dotenv import load_dotenv # used to import the token from the .env file
-#import asyncio
-#print(sys.path)
-#defining the global variables
-
-bot = discord.Bot()
+import os # default module
+from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()
+intents.members = False  # Enable the members intent
 
-intents = discord.Intents.all()
-client = discord.Client(command_prefix='!', intents=intents)
+bot = discord.Bot(intents=intents) # load all the variables from the env file
 
-#writing to the console to let the user know the bot is running
-@client.event
+
+@bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_ready():
-    print("Bot is now online!")
-#ending of online message
-ping = bot.create_group("latency", "ping")
-
-@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
-async def ping(ctx): # a slash command will be created with the name "ping"
-    await ctx.respond(f"Pong! Latency is {bot.latency}")
-
-greetings = bot.create_group("greetings", "Greet people")
-
-@greetings.command()
-async def hello(ctx):
-  await ctx.respond(f"Hello, {ctx.author}!")
-
-@greetings.command()
-async def bye(ctx):
-  await ctx.respond(f"Bye, {ctx.author}!")
+    print(f"{bot.user} is ready and online!")
     
-'''UNCOMMENT THIS CODE TO ENABLE THE BOT TO RESPOND TO MESSAGES gpt
-# https://jman4190.medium.com/how-to-build-a-discord-gpt3-chat-bot-with-openai-and-python-14177cd926f0
-# MAKE SURE TO UNCOMMENT THE IMPORTS AT THE TOP OF THE FILE
-# Make sure also to make this async in its own cog file so it  can hang and be responsive to other commands
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-  if message.content.startswith(‘Hello Mr. Botface’):
-    await message.channel.send(‘Howdy Stranger’)
-'''
+@bot.slash_command(name = "hello", description = "Say hello to the bot")
+async def hello(ctx):
+    await ctx.respond("Hey!")
+#bot.load_extension('cogs.Greetings') 
 
-client.run(TOKEN)
+
+#cogs_list = [
+ #   'cogs.greeting'  # greeting.py
+#]
+
+#or cog in cogs_list:
+ #   bot.load_extension(cog) 
+bot.run(os.getenv('TOKEN')) # run the bot with the token
